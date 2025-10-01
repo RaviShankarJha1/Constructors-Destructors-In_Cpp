d95ffcb573128ac4141549d7fea2725073e2be4a
RaviShankarJha1 <rjha81467@gmail.com>
2025-10-01 23:10:45 +0530

    Create Programe5.cpp

---
+++ Programe5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class CopyConst {
+    int x, y;
+
+public:
+   
+    CopyConst(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    
+    CopyConst(CopyConst &obj) {
+        x = obj.x;
+        y = obj.y;
+    }
+
+    void show() {
+        cout << "x = " << x << ", y = " << y << endl;
+    }
+};
+
+int main() {
+    CopyConst c1(10, 20);   
+    c1.show();              
+
+    CopyConst c2(c1);       
+    c2.show();             
+
+    return 0;
+}
